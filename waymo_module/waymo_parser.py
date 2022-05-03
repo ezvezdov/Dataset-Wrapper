13dd8761e810f62c997b3e6110b3d93e75e01e1a
Zviazdou, Yauheni <zviazyau@fel.cvut.cz>
2022-05-03 21:21:39 +0200

    Add Waymo categories Add Waymo labels

--- waymo_module/waymo_parser.py
+++ waymo_module/waymo_parser.py
@@ -21,6 +21,22 @@ from waymo_open_dataset import dataset_pb2 as open_dataset
 from waymo_open_dataset.protos import segmentation_metrics_pb2
 from waymo_open_dataset.protos import segmentation_submission_pb2
 
+import json
+
+# List of Waymo categories, id = index
+categories_list = ["UNDEFINED", "CAR", "TRUCK", "BUS", "OTHER_VEHICLE", "MOTORCYCLIST", "BICYCLIST", "PEDESTRIAN",
+                   "SIGN", "TRAFFIC_LIGHT", "POLE", "CONSTRUCTION_CONE", "BICYCLE", "MOTORCYCLE", "BUILDING",
+                   "VEGETATION", "TREE_TRUNK", "CURB", "ROAD", "LANE_MARKER", "OTHER_GROUND", "WALKABLE", "SIDEWALK"]
+#
+# categories_dict = {"UNDEFINED": "Undefined point", "CAR": "-", "TRUCK": "-", "OTHER_VEHICLE": "Other small vehicles (e.g. pedicab) and large vehicles (e.g. construction vehicles, RV, limo, tram).",
+#                    "MOTORCYCLIST": "-", "BICYCLIST": "-",
+#                    "PEDESTRIAN": "-", "SIGN": "-", "TRAFFIC_LIGHT": "-", "POLE": "Lamp post, traffic sign pole etc.",
+#                    "CONSTRUCTION_CONE": "Construction cone/pole.", "BICYCLE": "-", "MOTORCYCLE": "-",
+#                    "BUILDING": "-", "VEGETATION": "Bushes, tree branches, tall grasses, flowers etc.", "TREE_TRUNK": "-",
+#                    "CURB": "Curb on the edge of roads. This does not include road boundaries if there’s no curb.", "ROAD": "Surface a vehicle could drive on. This include the driveway connecting parking lot and road over a section of sidewalk.", "LANE_MARKER": "Marking on the road that’s specifically for defining lanes such as single/double white/yellow lines.", "OTHER_GROUND": "Marking on the road other than lane markers, bumps, cateyes, railtracks etc.",
+#                    "WALKABLE": "Most horizontal surface that’s not drivable, e.g. grassy hill, pedestrian walkway stairs etc.", "SIDEWALK": "Nicely paved walkable surface when pedestrians most likely to walk on."
+#                    }
+
 
 class WaymoParser(parser.Parser):
     def __init__(self, dataset_path: str):
@@ -48,13 +64,19 @@ class WaymoParser(parser.Parser):
                 counter += 1
             frame = open_dataset.Frame()
             frame.ParseFromString(bytearray(data.numpy()))
-            break
+            # TODO remove this construction and uncomment break
+            if frame.lasers[0].ri_return1.segmentation_label_compressed:
+                break
+            # break
         return frame
 
     def get_data(self, scene_number: int, frame_number: int):
         scene = self.__get_nth_scene(scene_number)
         frame = self.__get_nth_frame(scene, frame_number)
-        print(frame.context)
+        # print(frame.context)
+
+        (range_images, camera_projections, segmentation_labels,
+         range_image_top_pose) = frame_utils.parse_range_image_and_camera_projection(frame)
 
         coord = self.get_coordinates(frame)
 
@@ -66,8 +88,58 @@ class WaymoParser(parser.Parser):
 
         # TODO labels
         labels = []
+        print("KEYS ", segmentation_labels.keys())
+        if len(segmentation_labels) != 0:
+            print("Segmentation label was detected!")
+            point_labels = self.convert_range_image_to_point_cloud_labels(
+                frame, range_images, segmentation_labels)
+            # np.set_printoptions(threshold=np.inf)
+            # print(point_labels)
+            point_labels_all = np.concatenate(point_labels, axis=0)
+            print(point_labels_all)
+
+            # tmp_pcd = self.create_open3d_pc(point_labels_all)
+            # o3.visualization.draw_geometries([tmp_pcd])
 
         data = {'coordinates': coord, 'transformation_matrix': transformation_matrix, 'boxes': boxes, 'labels': labels}
+
+    def convert_range_image_to_point_cloud_labels(self, frame,
+                                                  range_images,
+                                                  segmentation_labels,
+                                                  ri_index=0):
+        """Convert segmentation labels from range images to point clouds.
+
+        Args:
+          frame: open dataset frame
+          range_images: A dict of {laser_name, [range_image_first_return,
+             range_image_second_return]}.
+          segmentation_labels: A dict of {laser_name, [range_image_first_return,
+             range_image_second_return]}.
+          ri_index: 0 for the first return, 1 for the second return.
+
+        Returns:
+          point_labels: {[N, 2]} list of 3d lidar points's segmentation labels. 0 for
+            points that are not labeled.
+        """
+        calibrations = sorted(frame.context.laser_calibrations, key=lambda c: c.name)
+        point_labels = []
+        for c in calibrations:
+            range_image = range_images[c.name][ri_index]
+            range_image_tensor = tf.reshape(
+                tf.convert_to_tensor(range_image.data), range_image.shape.dims)
+            range_image_mask = range_image_tensor[..., 0] > 0
+
+            if c.name in segmentation_labels:
+                sl = segmentation_labels[c.name][ri_index]
+                sl_tensor = tf.reshape(tf.convert_to_tensor(sl.data), sl.shape.dims)
+                print(sl_tensor)
+                sl_points_tensor = tf.gather_nd(sl_tensor, tf.where(range_image_mask))
+            else:
+                num_valid_point = tf.math.reduce_sum(tf.cast(range_image_mask, tf.int32))
+                sl_points_tensor = tf.zeros([num_valid_point, 2], dtype=tf.int32)
+
+            point_labels.append(sl_points_tensor.numpy())
+        return point_labels
 
     def get_coordinates(self, frame):
         (range_images, camera_projections, _, range_image_top_pose) = (
@@ -84,4 +156,6 @@ class WaymoParser(parser.Parser):
         return points_all
 
     def get_categories(self):
-        pass
+        with open(os.path.join(os.getcwd(), "waymo_module", "categories.json"), 'r') as f:
+            categories = json.load(f)
+        return categories
